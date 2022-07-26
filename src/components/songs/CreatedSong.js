79f3c53322fccfe14a246623c19e8efb3ac8e047
Tiffani Burk <greengurl1088@yahoo.com>
2022-07-26 11:08:52 -0500

    user can now delete a song

--- src/components/songs/CreatedSong.js
+++ src/components/songs/CreatedSong.js
@@ -1,8 +1,11 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+
 
 
 //create a component that will accept a single song object as a prop
 export const CreatedSong = ({id, name, artist}) => {
+
+    const navigate = useNavigate()
 
         return <section className="created-song">
         <div className="song-container"> 
@@ -14,7 +17,17 @@ export const CreatedSong = ({id, name, artist}) => {
             <Link to={`/songs/${id}`}>
                 View Song Details
             </Link>
+
+            <button onClick={() => {
+                fetch(`http://localhost:8088/songs/${id}`, {
+                    method: "DELETE"
+                })
+                .then((result) => {
+                  result.json().then(() => {
+                    navigate("/allsongs") //navs the user back to the song list
+                  })
+                })
+            }}>Delete</button>
         </div>
     </section>
- 
 }
